c592f251936775bfdd376743757410f66b6f190b
Oceon101 <osaosula@yahoo.com>
2024-09-02 09:36:25 +0100

    First commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import { Link } from "@mui/material";
+
+function Logo() {
+  return (
+    <Link href="/">
+      <img src="./comp-logo.png" alt="Caring Hands" />
+    </Link>
+  );
+}
+
+export default Logo;
