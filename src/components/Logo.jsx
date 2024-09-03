c9ac72c628f5e497ed0899c255e1f4cec59a9838
Oceon101 <osaosula@yahoo.com>
2024-09-03 09:37:03 +0100

    Getting logo url from a constant

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import { Link } from "@mui/material";
 
 function Logo() {
+  const logo_url = "./comp-logo.png";
   return (
     <Link href="/">
-      <img src="./comp-logo.png" alt="Caring Hands" />
+      <img src={logo_url} alt="Caring Hands" />
     </Link>
   );
 }
