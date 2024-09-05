b1a6bd03bb32822e09de63576c5213535b9ae381
Chris Boris Emefiele <greatecboris@gmail.com>
2024-09-05 04:20:47 -0400

    Add home page main menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,36 @@
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Groups2Icon from "@mui/icons-material/Groups2";
+import AirlineSeatFlatAngledTwoToneIcon from "@mui/icons-material/AirlineSeatFlatAngledTwoTone";
+import "../App.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: 6,
+      }}
+    >
+      <Button
+        sx={{ textTransform: "none" }}
+        size="large"
+        variant="contained"
+        color="primary"
+        startIcon={<Groups2Icon />}
+      >
+        Employee Management
+      </Button>
+      <Button
+        sx={{ textTransform: "none" }}
+        size="large"
+        variant="contained"
+        color="primary"
+        startIcon={<AirlineSeatFlatAngledTwoToneIcon />}
+      >
+        Patients Management
+      </Button>
+    </Box>
+  );
 };
-
 export default Home;
