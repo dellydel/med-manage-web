c14990c845acccb38529c573622cd8a68fc8f19b
Delvin Hall <dlvhall@gmail.com>
2024-09-09 19:12:05 -0500

    fix for Missing Admin component

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Home from './pages/Home'
-import './index.css'
-import Employees from './pages/Employees'
-import Patients from './pages/Patients'
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import "./index.css";
+import Employees from "./pages/Employees";
+import Patients from "./pages/Patients";
+import Admin from "./pages/Admin";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
+    element: <Home />,
   },
   {
     path: "/employees",
-    element: <Employees/>,
+    element: <Employees />,
   },
   {
     path: "/patients",
-    element: <Patients/>,
+    element: <Patients />,
   },
-	 {
-			path: "/admin",
-			element: <Admin/>,
-		},
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
