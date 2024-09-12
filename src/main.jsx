d80ded57cfb5bca51b19d0cd523d029b077c4e90
Delvin Hall <dlvhall@gmail.com>
2024-09-12 14:43:42 -0500

    refactor and cleanup, added layout page, fixed app bar

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,55 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Home from "./pages/Home";
-import "./index.css";
 import Employees from "./pages/Employees";
 import Patients from "./pages/Patients";
 import Admin from "./pages/Admin";
+import Layout from "./pages/Layout";
+
+const theme = createTheme({
+  components: {
+    MuiToolbar: {
+      styleOverrides: {
+        tall: {
+          height: 100,
+          minHeight: 100,
+        },
+      },
+    },
+  },
+});
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/employees",
-    element: <Employees />,
-  },
-  {
-    path: "/patients",
-    element: <Patients />,
-  },
-  {
-    path: "/admin",
-    element: <Admin />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/employees",
+        element: <Employees />,
+      },
+      {
+        path: "/patients",
+        element: <Patients />,
+      },
+      {
+        path: "/admin",
+        element: <Admin />,
+      },
+    ],
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,33 @@
-import NavBar from "../NavBar";
 import { Button, Box } from "@mui/material";
 import { Groups2, AirlineSeatFlatAngledTwoTone } from "@mui/icons-material";
-import "../App.css";
+//import "../App.css";
 
 const Home = () => {
   return (
-    <div>
-      <NavBar />
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: 6,
-        }}
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: 6,
+      }}
+    >
+      <Button
+        sx={{ textTransform: "none", py: 5, px: 8 }}
+        size="large"
+        variant="contained"
+        startIcon={<Groups2 />}
       >
-        <Button
-          sx={{ textTransform: "none", py: 5, px: 8 }}
-          size="large"
-          variant="contained"
-          startIcon={<Groups2 />}
-        >
-          Employee Management
-        </Button>
-        <Button
-          sx={{ textTransform: "none", py: 5, px: 8 }}
-          size="large"
-          variant="contained"
-          startIcon={<AirlineSeatFlatAngledTwoTone />}
-        >
-          Patients Management
-        </Button>
-      </Box>
-    </div>
+        Employee Management
+      </Button>
+      <Button
+        sx={{ textTransform: "none", py: 5, px: 8 }}
+        size="large"
+        variant="contained"
+        startIcon={<AirlineSeatFlatAngledTwoTone />}
+      >
+        Patients Management
+      </Button>
+    </Box>
   );
 };
 
