6e62b38c5dd6c2d5de734c898ab208757255def1
Oceon101 <osaosula@yahoo.com>
2024-09-15 21:42:56 +0100

    Added home link to the Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,15 +1,14 @@
 import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
+
 function Logo() {
   const logo_url = "./comp-logo.png";
   return (
-    <Box
-      component="img"
-      position="fixed"
-      sx={{ width: 204 }}
-      src={logo_url}
-      alt="Caring hands"
-    />
+    <Box component="div" position="fixed" sx={{ width: 240, height: 100 }}>
+      <Link to="/">
+        <img src={logo_url} alt="Caring hands" width={240} height={100} />
+      </Link>
+    </Box>
   );
 }
-
 export default Logo;
