9e6370af4dc1a060d10fc1744c46eb5a0e824068
Oceon101 <osaosula@yahoo.com>
2024-09-16 13:41:30 +0100

    Removed the link to suit the user story

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,10 @@
 import { Box } from "@mui/material";
-import { Link } from "react-router-dom";
 
 function Logo() {
   const logo_url = "./comp-logo.png";
   return (
     <Box component="div" position="fixed" sx={{ width: 240, height: 100 }}>
-      <Link to="/">
-        <img src={logo_url} alt="Caring hands" width={240} height={100} />
-      </Link>
+      <img src={logo_url} alt="Caring hands" width={240} height={100} />
     </Box>
   );
 }
