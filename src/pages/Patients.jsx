736f5254d6a438ace8da041e1480c59ee031b0d1
Chris Boris Emefiele <greatecboris@gmail.com>
2024-09-26 03:25:44 -0400

    added Patient Management - Grid Updates

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -5,17 +5,15 @@ import "ag-grid-community/styles/ag-theme-alpine.css";
 const Patients = () => {
   const [rowData] = useState([
     {
-      serialNo: 1,
       patientID: "CHCP1045",
       patientName: "GERALD ARNORLD",
-      patientEmail: "epipat.pe@gmail.com(CHCC0008)",
+      patientEmail: "epipat.pe@gmail.com",
       clinicianAssignedId: "CHCC0008",
       assignedTo: "ButtonHere",
-      status: "IN PROGRESS",
-    },
+      status: "IN PROGRESS"
+    }
   ]);
   const columnDefs = [
-    { field: "serialNo", headerName: "S No" },
     { field: "patientID", headerName: "Patient ID" },
     {
       field: "patientName",
@@ -23,21 +21,30 @@ const Patients = () => {
         params.value.replace(/\w\S*/g, (t) => {
           return t.charAt(0).toUpperCase() + t.substr(1).toLowerCase();
         }),
-      headerName: "Patient Name",
+      headerName: "Patient Name"
     },
     { field: "patientEmail", headerName: "Patient Email" },
-    { field: "clinicianAssignedId", headerName: "Clinician Assigned (ID)" },
+    { field: "clinicianAssignedId", headerName: "Clinician Assigned" },
     { field: "assignedTo", headerName: "Assigned To" },
-    { field: "status", headerName: "Status" },
+    { field: "status", headerName: "Status" }
   ];
+  const patientGridOptions = {
+    defaultColDef: {
+      resizable: true
+    },
+    pagination: true,
+    paginationPageSize: 10,
+    paginationPageSizeSelector: [10],
+    onGridReady: (params) => {
+      params.api.sizeColumnsToFit();
+    }
+  };
   return (
     <div className="ag-theme-alpine" style={{ height: "70vh", width: "80vw" }}>
       <AgGridReact
         rowData={rowData}
         columnDefs={columnDefs}
-        pagination={true}
-        paginationPageSize={10}
-        paginationPageSizeSelector={[10]}
+        gridOptions={patientGridOptions}
       />
     </div>
   );
