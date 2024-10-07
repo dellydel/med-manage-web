0ade36881475923fbff9f6c40659ad24c2b47560
Oceon101 <osaosula@yahoo.com>
2024-10-07 18:10:58 +0100

    Pateints url fetched from env

--- src/services/apiPatients.js
+++ src/services/apiPatients.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 export async function getPatients() {
-  const url =
-    "https://v5bgbb7bq3.execute-api.us-east-1.amazonaws.com/development/patients";
-  const { data } = await axios.get(url);
+  const url = import.meta.env.VITE_API_GATEWAY_BASE_URL;
+  const { data } = await axios.get(`${url}/patients`);
   return data;
 }
