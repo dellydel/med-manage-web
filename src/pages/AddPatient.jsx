5bd94515c59382b5a7ad2fdf3a3a8a65719fb27e
Chris Boris Emefiele <greatecboris@gmail.com>
2024-10-30 06:01:45 -0400

    Added new patient API call

--- src/pages/AddPatient.jsx
+++ src/pages/AddPatient.jsx
@@ -5,237 +5,303 @@ import {
   CardContent,
   Modal,
   TextField,
-  Typography,
+  Typography
 } from "@mui/material";
 import Grid from "@mui/material/Grid2";
-function AddPatient({ open, onClose }) {
+import { addPatient } from "../services/patients";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import Toast from "../components/Toast";
+const AddPatient = ({ open, onClose }) => {
+  const { register, handleSubmit, reset } = useForm();
+  const [openToast, setOpenToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+  const [toastSeverity, setToastSeverity] = useState("");
+  const queryClient = useQueryClient();
+  function handleReset() {
+    reset();
+  }
+  const { mutate, isPending } = useMutation({
+    mutationFn: addPatient,
+    onError: (error) => {
+      setToastSeverity("error");
+      setToastMessage(`${error.message}`);
+      setOpenToast(true);
+    },
+    onSuccess: (data) => {
+      setToastSeverity("success");
+      setToastMessage(data);
+      setOpenToast(true);
+      handleReset();
+      const timeout = setTimeout(() => onClose(), 3000);
+      return () => clearTimeout(timeout);
+    },
+    onSettled: async (_, error) => {
+      if (error) {
+        setToastSeverity("error");
+        setToastMessage(`${error.message}`);
+        setOpenToast(true);
+      } else {
+        await queryClient.invalidateQueries({ queryKey: ["patients"] });
+      }
+    }
+  });
+  const onSubmit = (data) => {
+    mutate(data);
+  };
   return (
     <Modal open={open} onClose={onClose}>
-      <Card style={{ maxWidth: "65%", margin: "10% auto" }}>
-        <Grid container bgcolor="#1976d2" color="#fff" padding={2}>
-          <Grid size={10}>
-            <Typography variant="h5" align="center" marginLeft={12}>
-              Add Patient
-            </Typography>
-          </Grid>
-          <Grid size={2}>
-            <Typography align="right">
-              <Close
-                onClick={onClose}
-                sx={{
-                  boxShadow: 3,
-                  "&.hover": { boxShadow: 8 },
-                  borderRadius: 28,
-                }}
-              />
-            </Typography>
-          </Grid>
-        </Grid>
-        <CardContent>
-          <form>
-            <Grid container spacing={1}>
-              <Grid size={12} item>
-                <Typography
-                  gutterBottom
-                  fontWeight={"fontWeightBold"}
-                  align="center"
-                >
-                  Patient Information (All fields are mandatory)
-                </Typography>
-              </Grid>
-              <Grid size={3} item>
-                First Name
-                <TextField
-                  label="First Name"
-                  type="text"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={3} item>
-                Last Name
-                <TextField
-                  type="text"
-                  label="Last Name"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={3} item>
-                Email
-                <TextField
-                  label="Email"
-                  type="email"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={3} item>
-                Mobile
-                <TextField
-                  label="Phone Number"
-                  type="number"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={3} item>
-                Address
-                <TextField
-                  label="Apt/Suit/H No. Street"
-                  type="text"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={3} item>
-                City
-                <TextField
-                  label="City"
-                  type="text"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={3} item>
-                Zip
-                <TextField
-                  label="Zip"
-                  type="text"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={3} item>
-                State
-                <TextField
-                  label="State"
-                  type="text"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={12} item>
-                <Typography
-                  gutterBottom
-                  fontWeight={"fontWeightBold"}
-                  align="center"
-                >
-                  Emergency Contact Information
-                </Typography>
-              </Grid>
-              <Grid size={3} item>
-                Contact First Name
-                <TextField
-                  label="First Name"
-                  type="text"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={3} item>
-                Contact Last Name
-                <TextField
-                  type="text"
-                  label="Last Name"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={3} item>
-                Relationship to Patient
-                <TextField
-                  label="Relationship"
-                  placeholder="Relationship"
-                  type="text"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={3} item>
-                Mobile No
-                <TextField
-                  label="Phone Number"
-                  type="number"
-                  size="small"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid size={3} item>
-                Address (Optional)
-                <TextField
-                  label="Apt/Suit/H No. Street"
-                  type="text"
-                  size="small"
-                  fullWidth
-                />
-              </Grid>
-              <Grid size={3} item>
-                City(Optional)
-                <TextField
-                  label="City"
-                  placeholder="City"
-                  type="text"
-                  size="small"
-                  fullWidth
-                />
-              </Grid>
-              <Grid size={3} item>
-                Zip code(Optional)
-                <TextField
-                  label="Zip"
-                  placeholder="Zip"
-                  type="text"
-                  size="small"
-                  fullWidth
-                />
-              </Grid>
-              <Grid size={3} item>
-                State(Optional)
-                <TextField
-                  label="State"
-                  placeholder="State"
-                  type="text"
-                  size="small"
-                  fullWidth
-                />
-              </Grid>
-              <Grid size={12} item align="center" marginTop={1}>
-                <Button
-                  type="reset"
-                  variant="contained"
-                  size="small"
-                  color="inherit"
-                >
-                  Cancel
-                </Button>
-                &nbsp;
-                <Button
-                  type="submit"
-                  variant="contained"
-                  size="small"
-                  color="success"
-                >
-                  Add
-                </Button>
-              </Grid>
+      <>
+        <Card style={{ maxWidth: "65%", margin: "10% auto" }}>
+          <Grid container bgcolor="#1976d2" color="#fff" padding={2}>
+            <Grid size={10}>
+              <Typography variant="h5" align="center" marginLeft={12}>
+                Add Patient
+              </Typography>
             </Grid>
-          </form>
-        </CardContent>
-      </Card>
+            <Grid size={2}>
+              <Typography align="right">
+                <Close
+                  onClick={onClose}
+                  sx={{
+                    boxShadow: 3,
+                    "&.hover": { boxShadow: 8 },
+                    borderRadius: 28
+                  }}
+                />
+              </Typography>
+            </Grid>
+          </Grid>
+          <CardContent>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Grid container spacing={1}>
+                <Grid size={12} item>
+                  <Typography
+                    gutterBottom
+                    fontWeight={"fontWeightBold"}
+                    align="center"
+                  >
+                    Patient Information (All fields are mandatory)
+                  </Typography>
+                </Grid>
+                <Grid size={3} item>
+                  First Name
+                  <TextField
+                    label="First Name"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("firstName")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  Last Name
+                  <TextField
+                    type="text"
+                    label="Last Name"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("lastName")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  Email
+                  <TextField
+                    label="Email"
+                    type="email"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("email")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  Mobile
+                  <TextField
+                    label="Phone Number"
+                    type="number"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("phone")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  Address
+                  <TextField
+                    label="Apt/Suit/H No. Street"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("streetAddress")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  City
+                  <TextField
+                    label="City"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("city")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  Zip
+                  <TextField
+                    label="Zip"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("zipCode")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  State
+                  <TextField
+                    label="State"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("state")}
+                  />
+                </Grid>
+                <Grid size={12} item>
+                  <Typography
+                    gutterBottom
+                    fontWeight={"fontWeightBold"}
+                    align="center"
+                  >
+                    Emergency Contact Information
+                  </Typography>
+                </Grid>
+                <Grid size={3} item>
+                  Contact First Name
+                  <TextField
+                    label="First Name"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("contactFirstName")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  Contact Last Name
+                  <TextField
+                    type="text"
+                    label="Last Name"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("contactLastName")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  Relationship to Patient
+                  <TextField
+                    label="Relationship"
+                    placeholder="Relationship"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("relationship")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  Mobile No
+                  <TextField
+                    label="Phone Number"
+                    type="number"
+                    size="small"
+                    fullWidth
+                    required
+                    {...register("contactPhone")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  Address (Optional)
+                  <TextField
+                    label="Apt/Suit/H No. Street"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    {...register("contactStreetAddress")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  City(Optional)
+                  <TextField
+                    label="City"
+                    placeholder="City"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    {...register("contactCity")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  Zip code(Optional)
+                  <TextField
+                    label="Zip"
+                    placeholder="Zip"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    {...register("contactZipCode")}
+                  />
+                </Grid>
+                <Grid size={3} item>
+                  State(Optional)
+                  <TextField
+                    label="State"
+                    placeholder="State"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    {...register("contactState")}
+                  />
+                </Grid>
+                <Grid size={12} item align="center" marginTop={1}>
+                  <Button
+                    type="reset"
+                    variant="contained"
+                    size="small"
+                    color="inherit"
+                  >
+                    Cancel
+                  </Button>
+                  &nbsp;
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    size="small"
+                    color="success"
+                    disabled={isPending}
+                  >
+                    {isPending ? "adding patient.." : "Add"}
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </CardContent>
+        </Card>
+        <Toast
+          open={openToast}
+          onClose={() => setOpenToast(false)}
+          severity={toastSeverity}
+          message={toastMessage}
+        />
+      </>
     </Modal>
   );
-}
+};
 export default AddPatient;
--- src/services/patients.js
+++ src/services/patients.js
@@ -1,6 +1,10 @@
 import { api } from "./axiosConfig";
-
 export const getPatients = async () => {
   const { data } = await api.get("/patients");
+  return data;
+};
+export const addPatient = async (newPatient) => {
+  const { data, error } = await api.post(`/patients`, newPatient);
+  if (error) throw new Error("Patient was not created due to API error");
   return data;
 };
