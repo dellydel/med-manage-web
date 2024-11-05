2130a1ea4d5cff32b0f61cc036e13372dd285f6f
Chris Boris Emefiele <greatecboris@gmail.com>
2024-11-04 21:24:47 -0500

    Added missing employee.js file

--- src/services/employees.js
+++ src/services/employees.js
@@ -3,14 +3,20 @@ export const getEmployees = async () => {
   const { data } = await api.get("/employees");
   return data;
 };
-
 export const getEmployeesByType = async (type) => {
   const { data } = await api.get(`/employees?type=${type}`);
   return data;
 };
-
 export const addEmployee = async (newEmployee) => {
   const { data, error } = await api.post("/employees", newEmployee);
   if (error) throw new Error("Employee could not be added");
+  return data;
+};
+export const deleteEmployee = async (employeeId) => {
+  const { data, error } = await api.delete(`/employees/${employeeId}`);
+  if (error)
+    throw new Error(
+      `Employee could not be deleted due to API error : ${error.message}`
+    );
   return data;
 };
